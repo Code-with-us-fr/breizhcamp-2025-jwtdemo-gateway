37a5010c96ef3f5f8a46a4cf53377fe8c35be743
MbarrouxCwu <mickael.barroux@codewithus.fr>
2025-05-27 15:30:40 +0200

    Step 4 : add rate-limiter

--- src/main/java/com/example/jwtdemo_gateway/JwtdemoGatewayApplication.java
+++ src/main/java/com/example/jwtdemo_gateway/JwtdemoGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.jwtdemo_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class JwtdemoGatewayApplication {
@@ -9,5 +12,14 @@ public class JwtdemoGatewayApplication {
     public static void main(final String[] args) {
         SpringApplication.run(JwtdemoGatewayApplication.class, args);
     }
+
+    @Bean
+    KeyResolver ipKeyResolver() {
+        return exchange -> {
+            final String ip = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+            return Mono.just(ip);
+        };
+    }
+
 
 }
