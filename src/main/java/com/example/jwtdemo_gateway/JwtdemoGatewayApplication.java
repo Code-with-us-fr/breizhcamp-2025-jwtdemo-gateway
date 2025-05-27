3fe33477484f1d55f231c66bc84367854f38d034
MbarrouxCwu <mickael.barroux@codewithus.fr>
2025-05-27 15:12:50 +0200

    Step 1 : add request header

--- src/main/java/com/example/jwtdemo_gateway/JwtdemoGatewayApplication.java
+++ src/main/java/com/example/jwtdemo_gateway/JwtdemoGatewayApplication.java
@@ -2,9 +2,6 @@ package com.example.jwtdemo_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class JwtdemoGatewayApplication {
@@ -12,11 +9,5 @@ public class JwtdemoGatewayApplication {
     public static void main(final String[] args) {
         SpringApplication.run(JwtdemoGatewayApplication.class, args);
     }
-
-    @Bean
-    KeyResolver ipKeyResolver() {
-        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
-    }
-
 
 }
